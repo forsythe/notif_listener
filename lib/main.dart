1773b49b04684ece484b60c9030b1ec89f135ee2
forsythe <su.heng620@gmail.com>
2020-12-19 13:30:35 +0800

    now with notification replay

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:android_notification_listener2/android_notification_listener2.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,11 +18,40 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   AndroidNotificationListener _notifications;
   StreamSubscription<NotificationEventV2> _subscription;
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+  final max32BitInt = pow(2, 31) - 1;
+  final List<RegExp> ignoreWhatsappMsgs = [
+    RegExp(r"^\d+ messages from \d+ chats$"),
+    RegExp(r"^\d+ new messages$"),
+    RegExp(r"^Checking for new messages$")
+  ];
 
   @override
   void initState() {
     super.initState();
     initPlatformState();
+
+    var initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
+    var initializationSettingsIOS = IOSInitializationSettings();
+    var initializationSettings = InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      // ignore: missing_return
+      onSelectNotification: (payload) {
+        showDialog(
+          context: context,
+          builder: (_) {
+            return AlertDialog(
+              title: Text("PayLoad"),
+              content: Text("Payload : $payload"),
+            );
+          },
+        );
+      },
+    );
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
@@ -29,15 +60,21 @@ class _MyAppState extends State<MyApp> {
   }
 
   void onData(NotificationEventV2 event) {
-
     print(event);
     print('converting package extra to json');
     var jsonDatax = json.decode(event.packageExtra);
+
     print(jsonDatax);
+    if (event.packageName != "com.whatsapp" ||
+        ignoreWhatsappMsgs.any((re) => re.hasMatch(event.packageMessage))) {
+      return;
+    }
+    _showNotificationWithoutSound(
+        event.timeStamp, event.packageText, event.packageMessage);
   }
 
   void startListening() {
-    _notifications = new AndroidNotificationListener();
+    _notifications = AndroidNotificationListener();
     try {
       _subscription = _notifications.notificationStream.listen(onData);
     } on NotificationExceptionV2 catch (exception) {
@@ -49,12 +86,33 @@ class _MyAppState extends State<MyApp> {
     _subscription.cancel();
   }
 
+  Future _showNotificationWithoutSound(
+      DateTime timestamp, String title, String content) async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'channel_id', 'Channel Name', 'Channel Description',
+        playSound: false,
+        importance: Importance.Max,
+        priority: Priority.High,
+        styleInformation: BigTextStyleInformation(''));
+    var iOSPlatformChannelSpecifics =
+        IOSNotificationDetails(presentSound: false);
+    var platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      hashValues(title.hashCode, content.hashCode),
+      title,
+      "[" + timestamp.toLocal().toString() + "] " + content,
+      platformChannelSpecifics,
+      payload: 'No_Sound',
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Notification Replayer example app'),
+          title: const Text('Notification Replayer'),
         ),
       ),
     );
