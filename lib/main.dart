ac7e24ec6abba5cda161bb2f7f764fce2c63e8bf
forsythe <su.heng620@gmail.com>
2020-12-19 17:16:34 +0800

    show time and pull to refresh

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'dart:math';
 import 'package:android_notification_listener2/android_notification_listener2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:intl/intl.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,13 +22,10 @@ class _MyAppState extends State<MyApp> {
   StreamSubscription<NotificationEventV2> _subscription;
   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
   List<ActiveNotification> _notifs = [];
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: true);
 
   final max32BitInt = pow(2, 31) - 1;
-  final List<RegExp> ignoreWhatsappMsgs = [
-    RegExp(r"^\d+ messages from \d+ chats$"),
-    RegExp(r"^\d+ new messages$"),
-    RegExp(r"^Checking for new messages$")
-  ];
 
   @override
   void initState() {
@@ -55,6 +54,8 @@ class _MyAppState extends State<MyApp> {
         );
       },
     );
+
+    _refreshNotificationDisplayList();
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
@@ -68,6 +69,12 @@ class _MyAppState extends State<MyApp> {
     var jsonDatax = json.decode(event.packageExtra);
 
     print(jsonDatax);
+    final List<RegExp> ignoreWhatsappMsgs = [
+      RegExp(r"^\d+ messages from \d+ chats$"),
+      RegExp(r"^\d+ new messages$"),
+      RegExp(r"^Checking for new messages$")
+    ];
+
     if (event.packageName != "com.whatsapp" ||
         ignoreWhatsappMsgs.any((re) => re.hasMatch(event.packageMessage))) {
       return;
@@ -75,14 +82,22 @@ class _MyAppState extends State<MyApp> {
     _showNotificationWithoutSound(
         event.timeStamp, event.packageText, event.packageMessage);
 
+    _refreshNotificationDisplayList();
+  }
+
+  void _refreshNotificationDisplayList() {
     flutterLocalNotificationsPlugin
         .resolvePlatformSpecificImplementation<
             AndroidFlutterLocalNotificationsPlugin>()
         ?.getActiveNotifications()
-        ?.then((value) => {
-              setState(() {
-                _notifs = value;
-              })
+        ?.then((value) => setState(() {
+              _notifs = value;
+            }))
+        ?.whenComplete(() => {
+              if (_refreshController.isRefresh)
+                _refreshController.refreshCompleted()
+              else if (_refreshController.isLoading)
+                _refreshController.loadComplete()
             });
   }
 
@@ -111,10 +126,12 @@ class _MyAppState extends State<MyApp> {
         android: androidPlatformChannelSpecifics,
         iOS: IOSNotificationDetails(),
         macOS: MacOSNotificationDetails());
+    final DateFormat dateFormatter = DateFormat('MMM dd, K:mm:ss a');
+
     await flutterLocalNotificationsPlugin.show(
       hashValues(timestamp.hashCode, title.hashCode, content.hashCode),
       title,
-      "[" + timestamp.toLocal().toString() + "] " + content,
+      "[" + dateFormatter.format(timestamp) + "] " + content,
       platformChannelSpecifics,
       payload: 'No_Sound',
     );
@@ -126,16 +143,53 @@ class _MyAppState extends State<MyApp> {
         _notifs.where((x) => x.body != null && x.title != null).toList();
     filteredNotifs.sort((x, y) => x.body.compareTo(y.body));
     return MaterialApp(
+      darkTheme: ThemeData.dark(),
       home: Scaffold(
           appBar: AppBar(
-            title: const Text('Notification Replayer'),
+            title: const Text('WhatsApp Notification Replayer'),
           ),
-          body: ListView.builder(
-              itemCount: filteredNotifs.length,
-              itemBuilder: (context, index) => ListTile(
-                    title: Text(filteredNotifs[index].title),
-                    subtitle: Text(filteredNotifs[index].body),
-                  ))),
+          body: Scrollbar(
+            child: SmartRefresher(
+              enablePullDown: true,
+              enablePullUp: false,
+              controller: _refreshController,
+              onRefresh: () => _refreshNotificationDisplayList(),
+              child: filteredNotifs.isEmpty
+                  ? (Center(
+                      child: Text("No displayed notifications"),
+                    ))
+                  : ListView.builder(
+                      itemCount: filteredNotifs.length,
+                      itemBuilder: (context, index) {
+                        var titleString = filteredNotifs[index].title;
+                        var bodyString = filteredNotifs[index].body;
+                        final RegExp parseBodyRegex =
+                            RegExp(r"^\[(.*), (\d+:\d+)(:.*)(AM|PM)\] (.*)");
+                        var parsedBody = parseBodyRegex.allMatches(bodyString);
+                        var hmTime = parsedBody.first.group(2);
+                        var amPm = parsedBody.first.group(4);
+                        var contentString = parsedBody.first.group(5);
+
+                        return Card(
+                          child: ListTile(
+                            visualDensity: VisualDensity.comfortable,
+                            leading: Text(titleString),
+                            title: Text(contentString.toString()),
+                            trailing: Tooltip(
+                              child: Text(
+                                  hmTime.toString() + " " + amPm.toString()),
+                              message: "Received " +
+                                  RegExp(r"^\[(.*)\](.*)")
+                                      .allMatches(bodyString)
+                                      .first
+                                      .group(1),
+                            ),
+                            dense: false,
+                          ),
+                        );
+                      }),
+            ),
+          )),
     );
   }
 }
